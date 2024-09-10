b1d2505f7f9b45aedc9e0d2e33a2dda919f1a82f
Cam Race <cameron.race@education.gov.uk>
2024-09-10 18:32:57 +0100

    add more examples

--- app.R
+++ app.R
@@ -1,18 +1,54 @@
 library(shiny)
 library(reactable)
+library(ggplot2)
+library(leaflet)
 
 ui <- fluidPage(
-  reactableOutput("table"),
-  reactableOutput("table2")
+  titlePanel("Testing out shinytest2 approaches"),
+  tabsetPanel(
+    tabPanel("Reactable", reactableOutput("clean_reactable"), reactableOutput("error_reactable")),
+    tabPanel("ggplot2", plotOutput("clean_ggplot"), plotOutput("error_ggplot")),
+    tabPanel("renderTable", tableOutput("clean_data_table"), tableOutput("error_data_table")),
+    tabPanel("Leaflet", leafletOutput("clean_map"), leafletOutput("error_map"))
+  )
 )
 
 server <- function(input, output, session) {
-  output$table <- renderReactable({
-    reactable(iris)
+  # Reactable outputs
+  output$clean_reactable <- renderReactable(reactable(iris))
+  output$error_reactable <- renderReactable(reactable(iris[0]))
+  
+  # ggplot2 output
+  output$clean_ggplot <- renderPlot({
+    ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+      geom_point() +
+      theme_minimal()
   })
   
-  output$table2 <- renderReactable({
-    reactable(iris[0])
+  output$error_ggplot <- renderPlot({
+    ggplot(iris[0], aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+      geom_point() +
+      theme_minimal()
+  })
+  
+  # renderTable output
+  output$clean_data_table <- renderTable(head(iris))
+  output$error_data_table <- renderTable(head(iris[0]))
+  
+  # Leaflet output
+  output$clean_map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = -1.5536, lat = 54.5245, zoom = 6) %>%
+      addMarkers(lng = -1.5536, lat = 54.5245, popup = "Darlington")
+  })
+  
+  # TODO: make an erroring leaflet map
+  output$error_map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = -1.5536, lat = 54.5245, zoom = "broken") %>%
+      addMarkers(lng = -1.5536, lat = 54.5245, popup = "Darlington")
   })
 }
 
--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,17 +1,32 @@
 library(shinytest2)
 
-test_that("Reactable table renders without errors", {
-  app <- AppDriver$new()
+# Check that a given table is present and has rendered rows of data
+reactable_rendered_rows <- function(table_name){
+  number_of_rendered_rows <- app$get_html(paste0("#", table_name)) |>
+    rvest::read_html() |> 
+    rvest::html_elements("div.rt-tr-group") |> 
+    length()
   
-  # Wait for the table to render
-  app$wait_for_idle(50)
-  
-  # Check for any error messages
+  return(number_of_rendered_rows)
+}
+
+# Create a test app
+app <- AppDriver$new()
+
+# Wait for the table to render
+app$wait_for_idle(50)
+
+test_that("There are no errors in the whole app", {
   expect_null(app$get_html(".shiny-output-error"))
   expect_null(app$get_html(".shiny-output-error-validation"))
-  
-  # Check that table is present
-  
-  
-  
 })
+
+test_that("Reactable table renders without errors", {
+  expect_gt(reactable_rendered_rows("clean_table"), 0)
+  expect_gt(reactable_rendered_rows("error_table"), 0)
+})
+
+# TODO: test out basic renderTable
+# TODO: test out ggplot2 with ggiraph
+# TODO: test out leaflet maps
+# TODO: test out file downloads (CSV / XLSX)
